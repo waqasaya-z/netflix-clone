670a1fb17de43290370a2cc67b695217c821713c
waqasaya-z <waqasayaz@icloud.com>
2024-07-15 18:49:33 +0500

    commit

--- magic.ts
+++ magic.ts
@@ -1,16 +1,16 @@
-import { Magic } from "magic-sdk"
+// import { Magic } from "magic-sdk"
 
 
 
-const createMagic = () => {
+// const createMagic = () => {
 
-  const API = process.env.MAGIC_API
+//   const API = process.env.MAGIC_API
 
-if (!API) {
-  throw new Error('MAGIC_API is not defined. Please set it in your environment.');
-}
+// if (!API) {
+//   throw new Error('MAGIC_API is not defined. Please set it in your environment.');
+// }
 
- return typeof window !== 'undefined' && new Magic(API)
+//  return typeof window !== 'undefined' && new Magic(API)
 
-}
-export const magic = createMagic()
+// }
+// export const magic = createMagic()
